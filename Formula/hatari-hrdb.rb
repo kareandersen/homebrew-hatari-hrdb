307fe874cd493acfe3b714bc04fc80730ae01af9
Kåre Andersen <kareandersen@gmail.com>
2025-05-19 11:29:04 +0200

    Add script and wrapper for HRDB Launch and CLI

--- Formula/hatari-hrdb.rb
+++ Formula/hatari-hrdb.rb
@@ -15,75 +15,121 @@ class HatariHrdb < Formula
   depends_on "readline"
 
   def install
-    # Build Hatari
+    (apps_dir = prefix/"Applications/hatari-hrdb").mkpath
+
     mkdir "build" do
       system "cmake", "..",
         "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.arch}",
+        "-DCMAKE_BUILD_TYPE=Release",
         *std_cmake_args
-        system "cmake", "--build", ".", "--parallel"
+      system "cmake", "--build", ".", "--parallel"
     end
 
-    # Prepare Applications subdir
-    (apps_dir = prefix/"Applications/hatari-hrdb").mkpath
-
-    # Rename Hatari.app and install
+    # Rename and install the app bundle
     mv "build/src/Hatari.app", "build/src/Hatari-HRDB.app"
     apps_dir.install "build/src/Hatari-HRDB.app"
 
-    # Build HRDB debugger
+    # Wrapper inside the .app bundle
+    in_bundle_wrapper = apps_dir/"Hatari-HRDB.app/Contents/MacOS/hatari-hrdb-wrapper.sh"
+    in_bundle_wrapper.write <<~EOS
+      #!/bin/bash
+      EXEC="$(dirname "$0")/Hatari"
+      ARGS=()
+      for arg in "$@"; do
+          case "$arg" in
+              "~" )   ARGS+=("$HOME") ;;
+              "~/"*)  ARGS+=("$HOME/${arg#~/}") ;;
+              * )     ARGS+=("$arg") ;;
+          esac
+      done
+      exec "$EXEC" "${ARGS[@]}"
+    EOS
+    in_bundle_wrapper.chmod 0755
+
+    # CLI wrapper in PATH
+    bin_wrapper = bin/"hatari-hrdb"
+    bin_wrapper.write <<~EOS
+      #!/bin/bash
+      WRAPPED="#{opt_prefix}/Applications/hatari-hrdb/Hatari-HRDB.app/Contents/MacOS/hatari-hrdb-wrapper.sh"
+      if [ ! -x "$WRAPPED" ]; then
+        echo "Hatari-HRDB error: $WRAPPED not found or not executable."
+        exit 1
+      fi
+      exec "$WRAPPED" "$@"
+    EOS
+    bin_wrapper.chmod 0755
+
+    # Build HRDB
     cd "tools/hrdb" do
       system "qmake", "."
       system "make"
       apps_dir.install "hrdb.app"
     end
 
-    # Install CLI symlinks
-    bin.install_symlink apps_dir/"Hatari-HRDB.app/Contents/MacOS/hatari" => "hatari-hrdb"
+    # CLI symlink for HRDB
     bin.install_symlink apps_dir/"hrdb.app/Contents/MacOS/hrdb"
 
+    # Linking script
     (link_script = bin/"link-hatari-hrdb.sh").write <<~EOS
-  #!/bin/bash
-  set -e
-
-  SOURCE_DIR="$(brew --prefix hatari-hrdb)/Applications/hatari-hrdb"
-  DEST_DIR="/Applications/hatari-hrdb"
-
-  mkdir -p "$DEST_DIR"
-
-  for app in "$SOURCE_DIR"/*.app; do
-    name=$(basename "$app")
-    target="$DEST_DIR/$name"
-    if [ ! -e "$target" ]; then
-      echo "Linking $name to $DEST_DIR"
-      ln -s "$app" "$target"
-    else
-      echo "$name already exists in $DEST_DIR"
-    fi
-  done
-
-  echo "Done! You should now see a folder in Applications called hatari-hrdb."
-EOS
-
+      #!/bin/bash
+      set -e
+      DEST_DIR="$HOME/Applications/hatari-hrdb"
+      [ "$1" == "--system" ] && DEST_DIR="/Applications/hatari-hrdb"
+      SOURCE_DIR="#{opt_prefix}/Applications/hatari-hrdb"
+      mkdir -p "$DEST_DIR"
+      for app in "$SOURCE_DIR"/*.app; do
+        name=$(basename "$app")
+        target="$DEST_DIR/$name"
+        if [ ! -e "$target" ]; then
+          echo "Linking $name to $DEST_DIR"
+          ln -s "$app" "$target"
+        else
+          echo "$name already exists in $DEST_DIR"
+        fi
+      done
+      echo "Done! Linked to: $DEST_DIR"
+    EOS
     link_script.chmod 0755
+
+    # Unlinking script
+    (unlink_script = bin/"unlink-hatari-hrdb.sh").write <<~EOS
+      #!/bin/bash
+      set -e
+      DEST_DIR="$HOME/Applications/hatari-hrdb"
+      [ "$1" == "--system" ] && DEST_DIR="/Applications/hatari-hrdb"
+      if [ ! -d "$DEST_DIR" ]; then
+        echo "No symlink directory found at: $DEST_DIR"
+        exit 0
+      fi
+      echo "Removing symlinks from: $DEST_DIR"
+      for link in "$DEST_DIR"/*.app; do
+        [ -L "$link" ] && echo "Removing $(basename "$link")" && rm "$link"
+      done
+      rmdir "$DEST_DIR" 2>/dev/null || true
+      echo "Done."
+    EOS
+    unlink_script.chmod 0755
   end
 
   def caveats
     <<~EOS
-    Hatari-HRDB and HRDB have been installed to:
-      #{opt_prefix}/Applications/hatari-hrdb/
+      Hatari-HRDB and HRDB have been installed to:
+        #{opt_prefix}/Applications/hatari-hrdb/
 
-    To link them into /Applications for easy access, run:
+      You can launch Hatari-HRDB via:
+        hatari-hrdb
 
-      link-hatari-hrdb.sh
+      Or launch the debugger via:
+        hrdb
 
-    This will symlink both Hatari-HRDB.app and hrdb.app into:
-      /Applications/hatari-hrdb/
+      To link them into your Applications folder:
+        link-hatari-hrdb.sh          # links to ~/Applications
+        sudo link-hatari-hrdb.sh --system  # links to /Applications
+
+      To remove the links:
+        unlink-hatari-hrdb.sh [--system]
+
     EOS
-  end
-
-  test do
-    assert_match "Hatari", shell_output("#{bin}/hatari-hrdb --help", 1)
-    assert_match "Usage", shell_output("#{bin}/hrdb --help", 1)
   end
 end
 
